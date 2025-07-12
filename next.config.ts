8d9c12d8b5b5a114b1a9308e41e57e3f0fce1789
Paul <pmi22@cornell.edu>
2025-07-11 22:54:47 -0400

    Disable eslint rule for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+    },
+  },
 };
 
 export default nextConfig;
